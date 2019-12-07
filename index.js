6a8b55b8efc21fd605d7207a5b725d064c8866b7
malgara <ayupov.m.b@gmail.com>
2019-12-07 16:20:25 +0600

    GraphQL resolvers

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import schema from './schema';
-import crypto from 'crypto';
-
+import resolvers from "./resolvers";
 const app = express();
 
 app.get('/', (req, res) => res.send('GraphQL is amanzing!'));
 
-class Friend {
-    constructor(id, { firstName, lastName, gender, language, email}) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.language = language;
-        this.email = email;
-    }
-}
-
-const FriendDataBase = {};
-const root = {
-    friend: () => {
-        return {
-            id: 2342342,
-            firstName: 'Madibek',
-            lastName: 'ayupov',
-            gender: 'Male',
-            language: 'Kazakh',
-            email: 'malgara@me.com'
-        }
-    },
-    createFriend: ({input}) => {
-        const id = crypto.randomBytes(10).toString('hex');
-        FriendDataBase[id] = input;
-        return new Friend(id, input);
-    }
-};
-
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
+    schema,
+    rootValue: resolvers,
     graphiql: true
 }))
 
--- schema.js
+++ schema.js
@@ -2,7 +2,7 @@ import {buildSchema} from 'graphql';
 
 const schema = buildSchema(`
     type Query {
-        friend: Friend
+        getFriend(id: ID): Friend
     }
     
     type Friend {
