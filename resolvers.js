878fc499a2fd1b34b4fe42c577cc9904a1b1f3cb
malgara <ayupov.m.b@gmail.com>
2019-12-07 16:21:30 +0600

    separate Resolvers

---
+++ resolvers.js
@@ -0,0 +1,26 @@
+import crypto from 'crypto';
+
+class Friend {
+    constructor(id, { firstName, lastName, gender, language, email}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.language = language;
+        this.email = email;
+    }
+}
+
+const FriendDataBase = {};
+const resolvers = {
+    getFriend: ({id}) => {
+        return new Friend(id, FriendDataBase[id]);
+    },
+    createFriend: ({input}) => {
+        const id = crypto.randomBytes(10).toString('hex');
+        FriendDataBase[id] = input;
+        return new Friend(id, input);
+    }
+};
+
+export default resolvers;
